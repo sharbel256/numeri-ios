d10dc36213a92af69d506b7e95e25e13cf7caa20
sharbel <sherbelhoma@hotmail.com>
2025-12-27 21:09:00 -0600

    Initial Commit

---
+++ numeri/numeriApp.swift
@@ -0,0 +1,17 @@
+//
+//  numeriApp.swift
+//  numeri
+//
+//  Created by Sharbel Homa on 12/27/25.
+//
+
+import SwiftUI
+
+@main
+struct numeriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ numeriTests/numeriTests.swift
@@ -0,0 +1,17 @@
+//
+//  numeriTests.swift
+//  numeriTests
+//
+//  Created by Sharbel Homa on 12/27/25.
+//
+
+import Testing
+@testable import numeri
+
+struct numeriTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
